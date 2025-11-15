076a0126e89a87b99c97e32672a8b69a84512653
bruceverchant <bruceverchant@gmail.com>
2025-11-14 23:00:12 -0500

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,14 +1,10 @@
-// This is a simplified example config file for quickstart
-// Some not frequently used features are omitted/commented out here
-// For a full-featured example, please refer to `uptime.config.full.ts`
-
-// Don't edit this line
 import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
-const pageConfig: PageConfig = {
-  // Title for your status page
+// ----------------------
+// Named Export for Next.js
+// ----------------------
+export const pageConfig: PageConfig = {
   title: "Stavan's Status Page",
-  // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://github.com/systaven', label: 'GitHub' },
     { link: 'https://blog.stavmb.me/', label: 'Blog' },
@@ -16,35 +12,23 @@ const pageConfig: PageConfig = {
   ],
 }
 
-const workerConfig: WorkerConfig = {
-  // Define all your monitors here
+export const workerConfig: WorkerConfig = {
   monitors: [
-    // Example HTTP Monitor
     {
-      // `id` should be unique, history will be kept if the `id` remains constant
       id: 'foo_monitor',
-      // `name` is used at status page and callback message
       name: 'Stavmb blog',
-      // `method` should be a valid HTTP Method
       method: 'GET',
-      // `target` is a valid URL
       target: 'https://blog.stavmb.me',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
       tooltip: 'This is a tooltip for this monitor',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
       statusPageLink: 'https://blog.stavmb.me',
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
       expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
       timeout: 10000,
-      // [OPTIONAL] headers to be sent
       headers: {
         'User-Agent': 'Uptimeflare',
         Authorization: 'Bearer YOUR_TOKEN_HERE',
       },
     },
 
-    // --- New Monitor 1: OpenList Halo 网盘 ---
     {
       id: 'openlist_halo_monitor',
       name: 'OpenList 网盘（Halo）',
@@ -60,7 +44,6 @@ const workerConfig: WorkerConfig = {
       },
     },
 
-    // --- New Monitor 2: AI Chat 网页 ---
     {
       id: 'ai_chat_web_monitor',
       name: 'AI 聊天网页',
@@ -75,57 +58,30 @@ const workerConfig: WorkerConfig = {
         Authorization: 'Bearer YOUR_TOKEN_HERE',
       },
     },
-
-    // Example TCP Monitor (Keep commented)
-    // {
-    //   id: 'test_tcp_monitor',
-    //   name: 'Example TCP Monitor',
-    //   // `method` should be `TCP_PING` for tcp monitors
-    //   method: 'TCP_PING',
-    //   // `target` should be `host:port` for tcp monitors
-    //   target: '1.2.3.4:22',
-    //   tooltip: 'My production server SSH',
-    //   statusPageLink: 'https://example.com',
-    //   timeout: 5000,
-    // },
   ],
-  // [Optional] Notification settings
+
   notification: {
-    // [Optional] Notification webhook settings, if not specified, no notification will be sent
-    // More info at Wiki: https://github.com/lyc8503/UptimeFlare/wiki/Setup-notification
     webhook: {
-      // [Required] webhook URL (example: Telegram Bot API)
       url: 'https://api.telegram.org/bot123456:ABCDEF/sendMessage',
-      // [Optional] HTTP method, default to 'GET' for payloadType=param, 'POST' otherwise
-      // method: 'POST',
-      // [Optional] headers to be sent
-      // headers: {
-      //   foo: 'bar',
-      // },
-      // [Required] Specify how to encode the payload
-      // Should be one of 'param', 'json' or 'x-www-form-urlencoded'
-      // 'param': append url-encoded payload to URL search parameters
-      // 'json': POST json payload as body, set content-type header to 'application/json'
-      // 'x-www-form-urlencoded': POST url-encoded payload as body, set content-type header to 'x-www-form-urlencoded'
       payloadType: 'x-www-form-urlencoded',
-      // [Required] payload to be sent
-      // $MSG will be replaced with the human-readable notification message
       payload: {
         chat_id: 12345678,
         text: '$MSG',
       },
-      // [Optional] timeout calling this webhook, in millisecond, default to 5000
       timeout: 10000,
     },
-    // [Optional] timezone used in notification messages, default to "Etc/GMT"
     timeZone: 'Asia/Shanghai',
-    // [Optional] grace period in minutes before sending a notification
-    // notification will be sent only if the monitor is down for N continuous checks after the initial failure
-    // if not specified, notification will be sent immediately
     gracePeriod: 5,
   },
 }
 
-// You can define multiple maintenances here
-// During maintenance, an alert will be shown at status page
-// Also, related downtime notifications will be sk
+export const maintenances: MaintenanceConfig[] = []
+
+// ----------------------
+// Default export for Uptimeflare Worker
+// ----------------------
+export default {
+  pageConfig,
+  workerConfig,
+  maintenances,
+}
